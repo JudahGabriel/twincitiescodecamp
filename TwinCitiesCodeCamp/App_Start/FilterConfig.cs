90be469e830c02b2a545494c80f6e993b3cefcf3
JudahGabriel <judahgabriel@gmail.com>
2016-03-14 17:15:26 -0500

    Home page and CSS skeleton

---
+++ TwinCitiesCodeCamp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TwinCitiesCodeCamp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TwinCitiesCodeCamp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwinCitiesCodeCamp.Startup))]
+namespace TwinCitiesCodeCamp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

d3ed5e5d4c3e6624d4f63b2db195a1e36056e63f
Judah Gabriel Himango <contact@bitshuva.com>
2016-03-14 20:19:25 -0500

    Added some models.

---
+++ TwinCitiesCodeCamp/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwinCitiesCodeCamp.Models
+{
+    public class Event
+    {
+        public string RegisterUrl { get; set; }
+        public DateTime DateTime { get; set; }
+        
+    }
+}
---
+++ TwinCitiesCodeCamp/Models/Talk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwinCitiesCodeCamp.Models
+{
+    public class Talk
+    {
+        public string Name { get; set; }
+    }
+}

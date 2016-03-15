17ee34c20e328dcacc17c90c26a3ce7f3e987ec8
JudahGabriel <judahgabriel@gmail.com>
2016-03-15 00:29:07 -0500

    Shared and index updated. Angular implemented.

---
+++ TwinCitiesCodeCamp/App/App.ts
@@ -0,0 +1,18 @@
+﻿namespace Tccc {
+    var modules = [
+        "ngRoute",
+        "ngAnimate",
+        "ui.bootstrap",
+        "ui.bootstrap.tpls"
+    ];
+
+    export var App = angular.module("TcccApp", modules);
+
+    App.config(["$routeProvider", function ($routeProvider: ng.route.IRouteProvider) {
+        $routeProvider
+            .when("/home", { templateUrl: "/App/Views/Home.html" })
+            .otherwise({ redirectTo: "/home" });
+    }]);
+    
+    //App.run(["$rootScope", function ($rootScope: ng.IRootScopeService) { }]);
+}
--- TwinCitiesCodeCamp/App_Start/BundleConfig.cs
+++ TwinCitiesCodeCamp/App_Start/BundleConfig.cs
@@ -11,13 +11,19 @@ namespace TwinCitiesCodeCamp
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js",
+                "~/Scripts/angular-animate.js",
+                "~/Scripts/angular-route.js",
+                "~/Scripts/angular-ui/ui-bootstrap.js",
+                "~/Scripts/angular-ui/ui-bootstrap-tpls.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/App/App.js",
+                "~/App/Common/*.js",
+                "~/App/Controllers/*.js",
+                "~/App/Models/*.js",
+                "~/App/Services/*.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",

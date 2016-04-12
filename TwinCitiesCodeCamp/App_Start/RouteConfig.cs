e5faa703ef16ee77435fb0c1ab96276da89f416a
JudahGabriel <judahgabriel@gmail.com>
2016-04-12 10:31:02 -0500

    Legacy session routing.

--- TwinCitiesCodeCamp/App_Start/RouteConfig.cs
+++ TwinCitiesCodeCamp/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace TwinCitiesCodeCamp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ TwinCitiesCodeCamp/Controllers/LegacyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwinCitiesCodeCamp.Data;
+using TwinCitiesCodeCamp.Models;
+
+namespace TwinCitiesCodeCamp.Controllers
+{
+    public class LegacyController : Controller
+    {
+        [Route("sessions/{seasonYear}")]
+        public ActionResult Sessions(string seasonYear)
+        {
+            using (var session = RavenContext.Db.OpenSession())
+            {
+                var seasonYearSpaces = seasonYear
+                    .Replace("Fall", "Fall ")
+                    .Replace("Spring", "Spring ");
+                var ev = session.Query<Event>().FirstOrDefault(e => e.SeasonYear == seasonYearSpaces);
+                if (ev == null)
+                {
+                    throw new ArgumentException("Couldn't find event for " + seasonYearSpaces);
+                }
+                
+                return View("Sessions", ev.Id);
+            }
+        }
+    }
+}
